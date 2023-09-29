c2c44c8c00fec983bd7bce05ff898d67890d5ba9
Yasuaki_Shintomi <shintomi.sh@gmail.com>
2023-09-29 15:08:57 +0900

    2023/09/29 col=2->4

--- resources/views/annualupdate/edit.blade.php
+++ resources/views/annualupdate/edit.blade.php
@@ -159,7 +159,7 @@
             <div class="row">
                 <div class="col-4">
                 </div>
-                <div class="col-2">
+                <div class="col-4">
                     <h5>進捗チェック</h5>
                 </div>
                 <div class="col-4">
@@ -181,7 +181,7 @@
             <div class="row">
                 <div class="col-4">
                 </div>
-                <div class="col-2">
+                <div class="col-4">
                     <h5>スケジュール</h5>
                 </div>
                 <div class="col-4">
@@ -203,7 +203,7 @@
             <div class="row">
                 <div class="col-4">
                 </div>
-                <div class="col-2">
+                <div class="col-4">
                     <h5>業務処理簿</h5>
                 </div>
                 <div class="col-4">
