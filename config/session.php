2a4960b171c6db9db23d67d55fddcf99b05e563d
Yasuaki_Shintomi <shintomi.sh@gmail.com>
2025-01-09 10:58:26 +0900

    2025/01/09 Secure属性がないのでtrue追加

--- config/session.php
+++ config/session.php
@@ -165,10 +165,10 @@ return [
     | By setting this option to true, session cookies will only be sent back
     | to the server if the browser has a HTTPS connection. This will keep
     | the cookie from being sent to you when it can't be done securely.
-    |
+    | HTTPSのCookieにSecure属性がないので追加
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE'),
+    'secure' => env('SESSION_SECURE_COOKIE', true), // 2025/01/09 true追加
 
     /*
     |--------------------------------------------------------------------------
