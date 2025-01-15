5ff70378d88ba6bdc3bbeacc4dbd579870619c19
Yasuaki_Shintomi <shintomi.sh@gmail.com>
2025-01-15 19:50:11 +0900

    2025/01/15 DumpDbAsSerializeLines

--- app/Console/Commands/DumpDbAsSerializeLines .php
+++ app/Console/Commands/DumpDbAsSerializeLines .php
@@ -20,7 +20,7 @@ class DumpDbAsSerializeLines extends BaseCommand
      *
      * @var string
      */
-    protected $signature = 'command:DumpDbAsSerializeLines'; // コマンドの名前を設定
+    protected $signature = 'command:DumpDbAsSerializeLines'; // コマンドの名前を設定 
 
     protected $name        = 'dev:dump-db-as-serialize-lines';
 
