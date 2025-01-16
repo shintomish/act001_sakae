436b566e19e1914db026ed500a1516a46e6a7f0a
Yasuaki_Shintomi <shintomi.sh@gmail.com>
2025-01-16 15:48:19 +0900

    2025/01/16 2011-2025

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,6 +78,6 @@
         </div>
     </body>
 
-    <p class="mt-5 mb-3 text-muted">AizenSolution Inc &copy; 2011-2024</p>
+    <p class="mt-5 mb-3 text-muted">Aizensolution Inc &copy; 2011-2025</p>
 
 @endsection
