5ae9306ae03c2eb1c17fbf3996f20b0dfe60cd0f
Yasuaki_Shintomi <shintomi.sh@gmail.com>
2025-01-16 10:24:45 +0900

    2025/01/16 git-test

--- app/Console/Commands/RestoreDbFromSerializeLines .php
+++ app/Console/Commands/RestoreDbFromSerializeLines .php
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class RestoreDbFromSerializeLines extends Command
 {
-    /**
+    /** 
      * The name and signature of the console command.
      *
      * @var string
